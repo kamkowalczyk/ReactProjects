6e2d84f56e1feeef88ffe25b82a140e283e39697
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-05-30 11:18:41 +0200

    pizzaWeb_Nav

---
+++ webpizza/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+      <img src="/public/img/logo.png" alt=""/>
+    </div>
+  )
+}
